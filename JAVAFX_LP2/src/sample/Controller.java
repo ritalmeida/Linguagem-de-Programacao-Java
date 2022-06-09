c70cabffb7bb2739900a20e2a8a38a74bc382bee
Rita A <55796419+ritalmeida@users.noreply.github.com>
2022-06-09 16:40:06 +0100

    Add files via upload

---
+++ JAVAFX_LP2/src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField verticesInput;
+
+    public void setVertices() {
+        System.out.println(verticesInput.getText());
+    }
+}
